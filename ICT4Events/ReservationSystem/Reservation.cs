704648b01e1ae4a1db1e1dddbcb3de5cb6361049
Ruud Schroën <schroenruud@gmail.com>
2015-04-07 14:42:13 +0200

    added classes for reservationsystem

---
+++ ICT4Events/ReservationSystem/Reservation.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AccountLibrary;
+
+namespace ReservationSystem
+{
+    class Reservation
+    {
+        public int ReservationID { get; private set; }
+        public Place Place { get; private set; }
+        public List<Account> Accounts { get; private set; }
+
+        public Reservation(int reservationID, Place place, List<Account> accounts)
+        {
+            ReservationID = reservationID;
+            Place = place;
+            Accounts = accounts;
+        }
+
+        public bool AddAccount(Account account){
+            Account exists = Accounts.Find(a => a.ID == account.ID);
+            if (exists != null)
+                return false;
+            Accounts.Add(account);
+            return true;
+        }
+
+        public bool RemoveAccount(Account account)
+        {
+            return Accounts.Remove(account);
+        }
+    }
+}
