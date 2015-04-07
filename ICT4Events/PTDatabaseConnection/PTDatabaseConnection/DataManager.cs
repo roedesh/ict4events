f60f23c3ab75689e5242dc046ce253893118cc16
Berry Verschueren <berryverschueren@gmail.com>
2015-04-07 10:38:04 +0200

    database en datamanager startups

---
+++ ICT4Events/PTDatabaseConnection/PTDatabaseConnection/DataManager.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PTDatabaseConnection
+{
+    class DataManager
+    {
+        DatabaseConnection connect;
+        public DataManager()
+        {
+            connect = new DatabaseConnection();
+            connect.Connect();
+        }
+        public void GetAccount()
+        {
+
+        }
+        public void GetAccount()
+        {
+
+        }
+        public void SetAcount()
+        {
+
+        }
+        public void GetEvent()
+        {
+
+        }
+        public void SetEvent()
+        {
+
+        }
+        public void GetItem()
+        {
+
+        }
+        public void SetItem()
+        {
+
+        }
+        public void GetReservation()
+        {
+
+        }
+        public void SetReservation()
+        {
+
+        }
+        public void GetPresence()
+        {
+
+        }
+        public void SetPresence()
+        {
+
+        }
+        public void SetReserved()
+        {
+
+        }
+        public void GetPost()
+        {
+
+        }
+        public void SetPost()
+        {
+
+        }
+    }
+}
---
+++ ICT4Events/PTDatabaseConnection/PTDatabaseConnection/DatabaseConnection.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//download allemaal http://www.oracle.com/technetwork/topics/dotnet/index-085163.html
+using Oracle.DataAccess.Client; 
+namespace PTDatabaseConnection
+{
+    class DatabaseConnection
+    {
+        OracleConnection connect;
+        public DatabaseConnection()
+        {
+            //
+        }
+        public void Connect()
+        {
+            connect = new OracleConnection();
+            connect.ConnectionString = "Data Source=<datasource>";
+            connect.Open();
+            Console.WriteLine("Connected to Oracle" + connect.ServerVersion);
+        }
+
+        public void Close()
+        {
+            connect.Close();
+            connect.Dispose();
+        }
+
+    }
+}
