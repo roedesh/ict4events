390e63fc59811c38338048d8da4c214930168a15
Jeroen Pullich <jeroen.pullich@hotmail.com>
2015-04-09 17:48:15 +0200

    meer klassen socialmedia

--- ICT4Events/SocialMediaSharingSystem/File.cs
+++ ICT4Events/SocialMediaSharingSystem/File.cs
@@ -6,7 +6,28 @@ using System.Threading.Tasks;
 
 namespace SocialMediaSharingSystem
 {
-    class File
+    public class File : Post
     {
+        private string filePath;
+        private int categorieID;
+
+        public string FilePath
+        {
+            get { return filePath; }
+            set { filePath = value; }
+        }
+
+        public int CategorieID
+        {
+            get { return categorieID; }
+            set { categorieID = value; }
+        }
+
+        public File(int postID, int uploaderID, string title, int amountOfLikes, int amountOfFlags, string filePath, int categorieID)
+            : base(postID, uploaderID, title, amountOfLikes, amountOfFlags)
+        {
+            this.filePath = filePath;
+            this.categorieID = categorieID;
+        }
     }
 }
