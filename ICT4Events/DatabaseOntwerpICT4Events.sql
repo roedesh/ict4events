f9c48326de3459d55d16b41307df495abb89f17c
Thom van Poppels <thom_vpoppel@hotmail.com>
2015-04-10 16:52:58 +0200

    sln?

--- ICT4Events/DatabaseOntwerpICT4Events.sql
+++ ICT4Events/DatabaseOntwerpICT4Events.sql
@@ -17,85 +17,85 @@ DROP TABLE Rental CASCADE CONSTRAINTS;
 DROP TABLE ItemRental CASCADE CONSTRAINTS;
 
 CREATE TABLE SwearWord
-(Word			varchar(20)		PRIMARY KEY
+(Word			varchar2(255 char)		PRIMARY KEY
 );
 
 
 CREATE TABLE Event
-(EventID		int				PRIMARY KEY,
-Location		varchar(20)		NOT NULL,
-StartDate		date			NOT NULL,
-EndDate			date			NOT NULL,
-Description		varchar(255)	NOT NULL,
-AdmissionFee	int				CHECK(AdmissionFee>0)
+(EventID		int					PRIMARY KEY,
+Location		varchar2(255 char)	NOT NULL,
+StartDate		date				NOT NULL,
+EndDate			date				NOT NULL,
+Description		varchar2(255 char)	NOT NULL,
+AdmissionFee	int					CHECK(AdmissionFee>0)
 );
 
 
 CREATE TABLE Account
-(AccountID		int				PRIMARY KEY,
-EventID			int				NOT NULL,
-Username		varchar(20)		UNIQUE,
-Password		varchar(20)		NOT NULL,
-FullName		varchar(30)		NOT NULL,
-Adress			varchar(255)	NOT NULL,
-City			varchar(80)		NOT NULL,
-PostalCode		varchar(6)		NOT NULL,
-DateOfBirth		date			NOT NULL,
-Email			varchar(20)		NOT NULL,
-Phonenumber		int				NOT NULL,
+(AccountID		int						PRIMARY KEY,
+EventID			int						NOT NULL,
+Username		varchar2(255 char)		UNIQUE,
+Password		varchar2(255 char)		NOT NULL,
+FullName		varchar2(255 char)		NOT NULL,
+Adress			varchar2(255 char)		NOT NULL,
+City			varchar2(255 char)		NOT NULL,
+PostalCode		varchar2(255 char)		NOT NULL,
+DateOfBirth		date					NOT NULL,
+Email			varchar2(255 char)		NOT NULL,
+Phonenumber		int						NOT NULL,
 CONSTRAINT FK_EventID1 FOREIGN KEY (EventID) REFERENCES Event (EventID)
 );
 
 CREATE TABLE Category
-(CategoryID	int				PRIMARY KEY,
-CategoryParent int				NULL,
-Name			varchar(20)		NOT NULL
+(CategoryID	int							PRIMARY KEY,
+CategoryParent int						NULL,
+Name			varchar2(255 char)		NOT NULL
 );
 
-CREATE TABLE FileTable
-(FileID			int				PRIMARY KEY,
-AccountID		int				NOT NULL,
-CategoryID		int				NOT NULL,
-DateTimeFile	timestamp		NOT NULL,
-Titel			varchar(20)		NOT NULL,
-FilePath		varchar(40)		NOT NULL,
-NumberOfLikes	int				DEFAULT 0,
-NumberOfFlags	int				DEFAULT 0,
+CREATE TABLE FileTable	
+(FileID			int						PRIMARY KEY,
+AccountID		int						NOT NULL,
+CategoryID		int						NOT NULL,
+DateTimeFile	timestamp				NOT NULL,
+Titel			varchar2(255 char)		NOT NULL,
+FilePath		varchar2(255 char)		NOT NULL,
+NumberOfLikes	int						DEFAULT 0,
+NumberOfFlags	int						DEFAULT 0,
 CONSTRAINT FK_AccountID1 FOREIGN KEY (AccountID) REFERENCES Account (AccountID),
 CONSTRAINT FK_Category1 FOREIGN KEY (CategoryID) REFERENCES Category (CategoryID)
 );
 
 
 CREATE TABLE CommentTable
-(CommentID		int				PRIMARY KEY,
-FileID			int				NOT NULL,
-AccountID		int				NOT NULL,
-CommentRecu		int				NULL,
-DateTimeComment	timestamp		NOT NULL,
-Titel			varchar(20)		NOT NULL,
-Message			varchar(200)	NULL,
-NumberOfLikes	int				DEFAULT 0,
-NumberOfFlags	int				DEFAULT 0,
+(CommentID		int						PRIMARY KEY,
+FileID			int						NOT NULL,
+AccountID		int						NOT NULL,
+CommentRecu		int						NULL,
+DateTimeComment	timestamp				NOT NULL,
+Titel			varchar2(255 char)		NOT NULL,
+Message			varchar2(255 char)		NULL,
+NumberOfLikes	int						DEFAULT 0,
+NumberOfFlags	int						DEFAULT 0,
 CONSTRAINT FK_AccountID2 FOREIGN KEY (AccountID) REFERENCES Account (AccountID),
 CONSTRAINT FK_FileID1 FOREIGN KEY (FileID) REFERENCES FileTable (FileID)
 );
 
 CREATE TABLE LikeOrFlag
-(LikeFlagID		int				PRIMARY KEY,
-AccountID		int				NOT NULL,
-CommentID		int				NULL,
-FileId			int				NULL,
-DateTimeLoF		timestamp		NOT NULL,
-TypeLOF			varchar(4)		CHECK(TypeLOF = 'LIKE' OR TypeLOF = 'FLAG'),
+(LikeFlagID		int						PRIMARY KEY,
+AccountID		int						NOT NULL,
+CommentID		int						NULL,
+FileId			int						NULL,
+DateTimeLoF		timestamp				NOT NULL,
+TypeLOF			varchar2(255 char)		CHECK(TypeLOF = 'LIKE' OR TypeLOF = 'FLAG'),
 CONSTRAINT FK_AccountID3 FOREIGN KEY (AccountID) REFERENCES Account (AccountID),
 CONSTRAINT FK_FileID2 FOREIGN KEY (FileID) REFERENCES FileTable (FileID),
 CONSTRAINT FK_CommentID1 FOREIGN KEY (CommentID) REFERENCES CommentTable (CommentId)
 );
 
 CREATE TABLE Role
-(RoleID			int				PRIMARY KEY,
-Name			varchar(20)		NOT NULL,
-Description		varchar(40)		NULL
+(RoleID			int						PRIMARY KEY,
+Name			varchar2(255 char)		NOT NULL,
+Description		varchar2(255 char)		NULL
 );
 
 CREATE TABLE Employee
@@ -107,24 +107,24 @@ CONSTRAINT FK_RoleID1 FOREIGN KEY (RoleID) REFERENCES Role (RoleID)
 );
 
 CREATE TABLE RFID
-(RFID			int				PRIMARY KEY,
-EndDate			date			NOT NULL,
-InUse			varchar(1)		CHECK(InUse = 'Y' OR InUse = 'N')
+(RFID			varchar2(255 char)		PRIMARY KEY,
+EndDate			date					NOT NULL,
+InUse			char		CHECK(InUse = 'Y' OR InUse = 'N')
 );
 
 CREATE TABLE Guest
-(GuestID		int				PRIMARY KEY,
-AccountID		int				NOT NULL,
-RFID			int				NOT NULL,
-IsPresent		varchar(1)		CHECK(IsPresent = 'Y' OR IsPresent = 'N'),
+(GuestID		int						PRIMARY KEY,
+AccountID		int						NOT NULL,
+RFID			varchar2(255 char)		NOT NULL,
+IsPresent		varchar2(255 char)		CHECK(IsPresent = 'Y' OR IsPresent = 'N'),
 CONSTRAINT FK_AccountID5 FOREIGN KEY (AccountID) REFERENCES Account (AccountID),
 CONSTRAINT FK_RFID FOREIGN KEY (RFID) REFERENCES RFID (RFID)
 );
 
 CREATE TABLE LocationType
-(LocationTypeID	int				PRIMARY KEY,
-Name			varchar(10)		NOT NULL,
-Description		varchar(50)		NULL,
+(LocationTypeID	int						PRIMARY KEY,
+Name			varchar2(255 char)		NOT NULL,
+Description		varchar2(255 char)		NULL,
 Price			int				CHECK(Price>0)
 );
 
@@ -134,8 +134,8 @@ LocationTypeID	int				NOT NULL,
 StartDate		date			NOT NULL,
 EndDate			date			NOT NULL,
 TotalAmount		int				CHECK(TotalAmount>0),
-PaymentStatus	varchar(8)		CHECK(PaymentStatus = 'PAID' OR PaymentStatus = 'NOT PAID'),
-CONSTRAINT FK_LocationTypeID FOREIGN KEY (LocationType) REFERENCES LocationType (LocationTypeID)
+PaymentStatus	varchar2(255 char)		CHECK(PaymentStatus = 'PAID' OR PaymentStatus = 'NOT PAID'),
+CONSTRAINT FK_LocationTypeID FOREIGN KEY (LocationTypeID) REFERENCES LocationType (LocationTypeID)
 );
 
 CREATE TABLE GuestReservation
@@ -147,11 +147,11 @@ CONSTRAINT FK_ReservationID1 FOREIGN KEY (ReservationID) REFERENCES Reservation 
 );
 
 CREATE TABLE Item
-(ItemID			int				PRIMARY KEY,
-Name			varchar(20)		NOT NULL,
-TypeItem		varchar(20)		NOT NULL,
-Stock			int				NOT NULL,
-Price			int				CHECK(Price>0)
+(ItemID			int						PRIMARY KEY,
+Name			varchar2(255 char)		NOT NULL,
+TypeItem		varchar2(255 char)		NOT NULL,
+Stock			int						NOT NULL,
+Price			int						CHECK(Price>0)
 );
 
 CREATE TABLE Rental
@@ -164,7 +164,7 @@ CONSTRAINT FK_GuestID2 FOREIGN KEY (GuestID) REFERENCES Guest (GuestID)
 );
 
 CREATE TABLE ItemRental
-(ItemRentalID	int       PRIMARY KEY,
+(ItemRentalID	int       		PRIMARY KEY,
 ItemID			int				NULL,
 RentalID		int				NULL,
 CONSTRAINT FK_RentalID1 FOREIGN KEY (RentalID) REFERENCES Rental (RentalID),
