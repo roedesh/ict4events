6ce7ec440130e73dc7125f75b5b139b89a49969a
Ruud Schroën <schroenruud@gmail.com>
2015-04-10 10:38:12 +0200

    reservation

--- ICT4Events/ReservationSystem/Form1.cs
+++ ICT4Events/ReservationSystem/Form1.cs
@@ -43,13 +43,7 @@ namespace ReservationSystem
         {
             ExtraPersons epForm = new ExtraPersons();
             var result = epForm.ShowDialog();
-            if (result == DialogResult.OK)
-            {
-                string val = epForm.ReturnValue1;            //values preserved after close
-                string dateString = epForm.ReturnValue2;
-                //Do something here with these values
-
-            }
+            
         }
     }
 }
