869d44cf533b9750ce78ba3257510bf40fa27aac
Thom van Poppels <thom_vpoppel@hotmail.com>
2015-04-10 09:59:51 +0200

    GUI scanners

---
+++ ICT4Events/EntryControlSystem/SuperManager.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntryControlSystem
+{
+    class SuperManager
+    {
+        public void SuperMager()
+        { }
+
+        public bool CheckCode(string code)
+        { }
+
+        public bool CheckPresence(int accountID)
+        { }
+
+        public bool SetPresence(int accountID)
+        { }
+    }
+}
