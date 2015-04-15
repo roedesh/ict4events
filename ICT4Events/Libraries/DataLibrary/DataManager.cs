eefa987ae19da31d3bb6f194c67d7f3e4a7e0bb8
Berry Verschueren <berryverschueren@gmail.com>
2015-04-15 10:34:30 +0200

    datamanager geupdate

--- ICT4Events/Libraries/DataLibrary/DataManager.cs
+++ ICT4Events/Libraries/DataLibrary/DataManager.cs
@@ -58,7 +58,7 @@ namespace DataLibrary
             catch (OracleException ex)
             {
             
-                Console.WriteLine("Exception Source: " + ex.Source);
+                Console.WriteLine("Exception Source: " + ex);
             }
             finally
             {
@@ -68,32 +68,32 @@ namespace DataLibrary
         }
         public List<Dictionary<string, string>> GetGuestAccount(int ID)
         {
-            string query = String.Format("SELECT * FROM Account a, Guest g WHERE a.AccountID = g.AccountID AND a.AccountID = '{0}'", ID);
+            string query = String.Format("SELECT * FROM Account a, Guest g WHERE a.AccountID = g.AccountID AND a.AccountID = {0}", ID);
             result = XCTReader(query);
             return result;
         }
         public List<Dictionary<string, string>> GetGuestAccount(string name)
         {
-            string query = String.Format("SELECT * FROM Account a, Guest g WHERE a.AccountID = g.AccountID AND a.Username = '{0}'", name);
+            string query = String.Format("SELECT * FROM Account a, Employee g WHERE a.AccountID = g.AccountID AND a.AccountID = '{0}'", name);
             result = XCTReader(query);
             return result;
         }
         public List<Dictionary<string, string>> GetEmployeeAccount(int ID)
         {
-            string query = String.Format("SELECT * FROM ACCOUNT A,EMPLOYEE E WHERE A.ACCOUNTID = E.ACCOUNTID AND A.ACCOUNTID = {0}", ID);
+            string query = String.Format("SELECT * FROM Account a, Employee g WHERE a.AccountID = g.AccountID AND a.AccountID = {0}", ID);
             result = XCTReader(query);
             return result;
         }
         public List<Dictionary<string, string>> GetEmployeeAccount(string name)
         {
-            string query = String.Format("SELECT * FROM ACCOUNT A,EMPLOYEE E WHERE A.ACCOUNTID = E.ACCOUNTID AND A.USERNAME = {0}", name);
+            string query = String.Format("SELECT * FROM Account a, Employee g WHERE a.AccountID = g.AccountID AND a.AccountID = '{0}'", name);
             result = XCTReader(query);
             return result;
         }
         public void SetGuestAccount(List<string> account, List<string> guest)
         {
             string date = String.Format("TO_DATE('{0}', 'DD-MM-YYYY')", account[8]);
-            string query = String.Format("INSERT INTO ACCOUNT VALUES({0},{1},'{2}','{3}','{4}','{5}','{6}','{7}',{8},'{9}','{10}')"
+            string query = String.Format("INSERT INTO ACCOUNT VALUES({0},'{1}','{2}','{3}','{4}','{5}','{6}','{7}',{8},'{9}','{10}')"
                 , account[0], account[1], account[2], account[3]
                 , account[4], account[5], account[6], account[7]
                 , date, account[9],account[10]);
@@ -103,16 +103,16 @@ namespace DataLibrary
         }
         public void SetEmployeeAccount(List<string> account, List<string> employee)
         {
-            string query = "SELECT MAX(ACCOUNTID) FROM ACCOUNT";
+            string query = "SELECT MAX(AccountID) FROM Account";
             result = XCTReader(query);
             int ID = Convert.ToInt32(result) + 1;
             string date = String.Format("TO_DATE('{0}', 'yyyy/mm/dd hh24:mi:ss')", account[8]);
-            query = String.Format("INSERT INTO ACCOUNT VALUES({0}{1}{2}{3}{4}{5}{6}{7}{8}{9})"
+            query = String.Format("INSERT INTO Account VALUES({0},'{1}','{2}','{3}','{4}','{5}','{6}','{7}',{8},'{9}')"
                 , ID, account[1], account[2], account[3]
                 , account[4], account[5], account[6], account[7]
                 , date, account[9]);
             XCTNonQuery(query);
-            query = String.Format("INSERT INTO GUEST VALUES({0}{1}{2})"
+            query = String.Format("INSERT INTO Employee VALUES({0},{1},'{2}')"
                 , employee[0], employee[1], employee[2]);
             XCTNonQuery(query);
         }
@@ -124,19 +124,19 @@ namespace DataLibrary
         }
         public List<Dictionary<string, string>> GetAllEmployees()
         {
-            string query = "SELECT * FROM EMPLOYEE";
+            string query = "SELECT * FROM Employee g, Account a WHERE a.AccountID = g.AccountID ";
             result = XCTReader(query);
             return result;
         }
         public List<Dictionary<string, string>> GetAllEvents()
         {
-            string query = "SELECT * FROM EVENT";
+            string query = "SELECT * FROM Event";
             result = XCTReader(query);
             return result;
         }
         public List<Dictionary<string, string>> GetEvent(string ID)
         {
-            string query = String.Format("SELECT * FROM EVENT WHERE EVENTID = {0}", ID);
+            string query = String.Format("SELECT * FROM Event WHERE EventID = {0}", ID);
             result = XCTReader(query);
             return result;
         }
@@ -151,30 +151,30 @@ namespace DataLibrary
         }
         public void DeleteGuest(string ID)
         {
-            string query = String.Format("DELETE * FROM GUEST G, ACCOUNT A WHERE A.ACCOUNTID = G.ACCOUNTID AND A.ACCOUNTID = {0}", ID);
+            string query = String.Format("DELETE * FROM Guest G, Account A WHERE A.AccountID = G.AccountID AND A.AccountID = {0}", ID);
             XCTNonQuery(query);
         }
         public void DeleteEmployee(string ID)
         {
-            string query = String.Format("DELETE * FROM EMPLOYEE G, ACCOUNT A WHERE A.ACCOUNTID = G.ACCOUNTID AND A.ACCOUNTID = {0}", ID);
+            string query = String.Format("DELETE * FROM Employee G, Account A WHERE A.AccountID = G.AccountID AND A.AccountID = {0}", ID);
             XCTNonQuery(query);
         }
         public void DeleteEvent(string ID)
         {
-            string query = String.Format("DELETE FROM Account WHERE EVENTID = {0}", ID);
+            string query = String.Format("DELETE FROM Account WHERE EventID = {0}", ID);
             XCTNonQuery(query);
             query = String.Format("DELETE FROM Event WHERE EventID = {0}", ID);
             XCTNonQuery(query);
         }
         public void UpdateGuest(List<string> guest)
         {
-            string query = String.Format("UPDATE GUEST SET GUESTID = {0}, ACCOUNTID = {1}, RFID = {2}, ISPRESENT = {3};"
+            string query = String.Format("UPDATE Guest SET GuestID = {0}, AccountID = {1}, RFID = '{2}', IsPresent = '{3}';"
                 , guest[0], guest[1], guest[2], guest[3]);
             XCTNonQuery(query);
         }
         public void UpdateEmployee(List<string> employee)
         {
-            string query = String.Format("UPDATE EMPLOYEE SET EMPLOYEEID = {0}, ACCOUNTID = {1}, ROLEID = {2};"
+            string query = String.Format("UPDATE Employee SET EMPLOYEEID = {0}, ACCOUNTID = {1}, ROLEID = '{2}';"
                 , employee[0], employee[1], employee[2]);
             XCTNonQuery(query);
         }
@@ -182,14 +182,14 @@ namespace DataLibrary
         {
             string dateStart = String.Format("TO_DATE('{0}', 'DD-MM-YYYY')", eventinfo[2]);
             string dateEnd = String.Format("TO_DATE('{0}', 'DD-MM-YYYY')", eventinfo[3]);
-            string query = String.Format("UPDATE EVENT SET EVENTID = {0}, LOCATION = {1}, STARTDATE = {2}, ENDDATE = {3}, DESCRIPTION = {4}, ADMISSIONFEE = {5};"
+            string query = String.Format("UPDATE Event SET EVENTID = {0}, LOCATION = '{1}', STARTDATE = {2}, ENDDATE = {3}, DESCRIPTION = '{4}', ADMISSIONFEE = '{5}';"
                 , eventinfo[0], eventinfo[1], dateStart, dateEnd, eventinfo[4], eventinfo[5]);
             XCTNonQuery(query);
         }
         public void UpdateAccount(List<string> account)
         {
             string date = String.Format("TO_DATE('{0}', 'yyyy/mm/dd hh24:mi:ss')", account[8]);
-            string query = String.Format("UPDATE  ACCOUNT SET ACCOUNTID = {0}, EVENTID = {1}, USERNAME = {2}, PASSWORD = {3}, FULLNAME = {4}, ADRESS = {5}, CITY = {6}, POSTALCODE = {7}, DATEOFBIRTH = {8}, EMAIL = {9}, PHONENUMBER = {10}"
+            string query = String.Format("UPDATE  Account SET ACCOUNTID = {0}, EVENTID = {1}, USERNAME = '{2}', PASSWORD = '{3}', FULLNAME = '{4}', ADRESS = '{5}', CITY = '{6}', POSTALCODE = '{7}', DATEOFBIRTH = {8}, EMAIL = '{9}', PHONENUMBER = '{10}'"
                 , account[0], account[1], account[2], account[3]
                 , account[4], account[5], account[6], account[7]
                 , date, account[9]);
@@ -197,22 +197,22 @@ namespace DataLibrary
         }
         public void UpdateLocation(List<string> location)
         {
-            string query = String.Format("UPDATE LOCATIONTYPE LOCATIONTYPEID = {0}, NAME = {1}, DESCRIPTION = {2}, PRICE = {3}"
+            string query = String.Format("UPDATE LocationType LOCATIONTYPEID = {0}, NAME = '{1}', DESCRIPTION = '{2}', PRICE = '{3}'"
                 , location[0], location[1], location[2], location[3]);
             XCTNonQuery(query);
         }
         public List<Dictionary<string, string>> GetItem(string ID)
         {
-            string query = String.Format("SELECT * FROM ITEM WHERE ITEMID = {0}", ID);
+            string query = String.Format("SELECT * FROM Item WHERE ITEMID = {0}", ID);
             result = XCTReader(query);
             return result;
         }
         public void SetItem(List<string> item)
         {
-            string query = "SELECT MAX(ITEMID) FROM ITEM";
+            string query = "SELECT MAX(ITEMID) FROM Item";
             result = XCTReader(query);
             int ID = Convert.ToInt32(result) + 1;
-            query = String.Format("INSERT INTO ITEM VALUES({0}{1}{2}{3}{4})"
+            query = String.Format("INSERT INTO Item VALUES({0},'{1}','{2}','{3}','{4}')"
                 , ID, item[1], item[2], item[3]
                 , item[4]);
             XCTNonQuery(query);
@@ -225,12 +225,12 @@ namespace DataLibrary
         }
         public void SetReservation(List<string> reservation)
         {
-            string query = "SELECT MAX(RESERVATIONID) FROM RESERVATION";
+            string query = "SELECT MAX(RESERVATIONID) FROM Reservation";
             result = XCTReader(query);
             int ID = Convert.ToInt32(result) + 1;
             string dateStart = String.Format("TO_DATE('{0}', 'yyyy/mm/dd hh24:mi:ss')", reservation[2]);
             string dateEnd = String.Format("TO_DATE('{0}', 'yyyy/mm/dd hh24:mi:ss')", reservation[3]);
-            query = String.Format("INSERT INTO RESERVATION VALUES({0}{1}{2}{3}{4}{5})"
+            query = String.Format("INSERT INTO Reservation VALUES({0},'{1}',{2},{3},'{4}','{5}')"
                 , ID, reservation[1], dateStart, dateEnd
                 , reservation[4], reservation[5]);
             XCTNonQuery(query);
@@ -240,7 +240,7 @@ namespace DataLibrary
             string query = "SELECT MAX(GUESTRESERVATIONID) FROM GUESTRESERVATION";
             result = XCTReader(query);
             int ID = Convert.ToInt32(result) + 1;
-            query = String.Format("INSERT INTO GUESTRESERVATION VALUES({0}{1}{2})", ID, RID, PID);
+            query = String.Format("INSERT INTO GUESTRESERVATION VALUES({0},{1},{2})", ID, RID, PID);
             XCTNonQuery(query);
         }
         public List<Dictionary<string, string>> GetPresence(string ID)
@@ -251,7 +251,7 @@ namespace DataLibrary
         }
         public void SetPresence(string Pvalue, string ID)
         {
-            string query = String.Format("INSERT INTO GUEST (ISPRESENT) VALUES({0}) WHERE ACCOUNTID = {1}", Pvalue, ID);
+            string query = String.Format("INSERT INTO GUEST (ISPRESENT) VALUES('{0}') WHERE ACCOUNTID = {1}", Pvalue, ID);
             XCTNonQuery(query);
         }
         public void SetFile(List<string> file)
@@ -259,7 +259,7 @@ namespace DataLibrary
             string query = "SELECT MAX(FILEID) FROM FILETABLE";
             result = XCTReader(query);
             int ID = Convert.ToInt32(result) + 1;
-            query = String.Format("INSERT INTO FILETABLE VALUES({0}{1}{2}{3}{4}{5}{6}{7})"
+            query = String.Format("INSERT INTO FILETABLE VALUES({0},{1},'{2}','{3}','{4}','{5}','{6}','{7}')"
                 , ID, file[1], file[2], file[3]
                 , file[4], file[5], file[6], file[7]);
             XCTNonQuery(query);
@@ -269,9 +269,10 @@ namespace DataLibrary
             string query = "SELECT MAX(COMMENTID) FROM COMMENT";
             result = XCTReader(query);
             int ID = Convert.ToInt32(result) + 1;
-            query = String.Format("INSERT INTO COMMENTTABLE VALUES({0}{1}{2}{3}{4}{5}{6}{7}{8})"
+            string dateStart = String.Format("TO_DATE('{0}', 'yyyy/mm/dd hh24:mi:ss')", comment[4]);
+            query = String.Format("INSERT INTO COMMENTTABLE VALUES({0},'{1}','{2}','{3}',{4},'{5}','{6}','{7}','{8}')"
                 , ID, comment[1], comment[2], comment[3]
-                , comment[4], comment[5], comment[6], comment[7]
+                , dateStart, comment[5], comment[6], comment[7]
                 , comment[8]);
             XCTNonQuery(query);
         }
@@ -280,7 +281,7 @@ namespace DataLibrary
             string query = "SELECT MAX(LIKEFLAGID) FROM LIKEORFLAG";
             result = XCTReader(query);
             int ID = Convert.ToInt32(result) + 1;
-            query = String.Format("INSERT INTO LIKEORFLAG VALUES({0}{1}{2}{3}{4}{5})"
+            query = String.Format("INSERT INTO LIKEORFLAG VALUES({0},'{1}','{2}','{3}','{4}','{5}')"
                 , ID, likeorflag[1], likeorflag[2], likeorflag[3]
                 , likeorflag[4], likeorflag[5]);
             XCTNonQuery(query);
@@ -305,16 +306,17 @@ namespace DataLibrary
         }
         public void UpdateFile(List<string> file)
         {
-            string query = String.Format("UPDATE FILETABLE SET FILEID = {0}, ACCOUNTID = {1}, CATEGORYID = {2}, DATETIMEFILE = {3}, TITEL = {4}, FILEPATH = {5}, NUMBEROFLIKES = {6}, NUMBEROFFLAGS = {7}"
+            string query = String.Format("UPDATE FILETABLE SET FILEID = {0}, ACCOUNTID = {1}, CATEGORYID = '{2}', DATETIMEFILE = '{3}', TITEL = '{4}', FILEPATH = '{5}', NUMBEROFLIKES = '{6}', NUMBEROFFLAGS = '{7}'"
                 , file[0], file[1], file[2], file[3]
                 , file[4], file[5], file[6], file[7]);
             XCTNonQuery(query);
         }
         public void UpdateComment(List<string> file)
         {
-            string query = String.Format("UPDATE COMMENTTABLE SET COMMENTID = {0}, FILEID = {1}, ACCOUNTID = {2}, COMMENTRECU = {3}, DATETIMECOMMENT = {4}, TITEL = {5}, MESSAGE = {6}, NUMBEROFLIKES = {7}, NUMBEROFFLAGS = {8}"
+            string dateStart = String.Format("TO_DATE('{0}', 'yyyy/mm/dd hh24:mi:ss')", file[4]);
+            string query = String.Format("UPDATE COMMENTTABLE SET COMMENTID = {0}, FILEID = {1}, ACCOUNTID = {2}, COMMENTRECU = '{3}', DATETIMECOMMENT = {4}, TITEL = '{5}', MESSAGE = '{6}', NUMBEROFLIKES = '{7}', NUMBEROFFLAGS = '{8}'"
                 , file[0], file[1], file[2], file[3]
-                , file[4], file[5], file[6], file[7]);
+                , dateStart, file[5], file[6], file[7]);
             XCTNonQuery(query);
         }
         public void DeleteFile(string ID)
@@ -339,7 +341,7 @@ namespace DataLibrary
             string query = "SELECT MAX(CATEGORYID) FROM CATEGORY";
             result = XCTReader(query);
             int ID = Convert.ToInt32(result) + 1;
-            query = String.Format("INSERT INTO CATEGORY VALUES({0}{1}{2})"
+            query = String.Format("INSERT INTO CATEGORY VALUES({0},{1},'{2}')"
                 , ID, category[1], category[2]);
             XCTNonQuery(query);
         }
@@ -357,7 +359,7 @@ namespace DataLibrary
         }
         public void UpdateCategory(List<string> category)
         {
-            string query = String.Format("UPDATE CATEGORY SET CATEGORYID = {0}, CATEGORYPARENT = {1}, NAME = {2})"
+            string query = String.Format("UPDATE CATEGORY SET CATEGORYID = {0}, CATEGORYPARENT = {1}, NAME = '{2}')"
                 , category[0], category[1], category[2]);
             XCTNonQuery(query);
         }
@@ -382,7 +384,7 @@ namespace DataLibrary
         }
         public void UpdateItem(List<string> item)
         {
-            string query = String.Format("UPDATE ITEM SET ITEMID = {0}, NAME = {1}, TYPEITEM = {2}, STOCK = {3}, PRICE = {4})"
+            string query = String.Format("UPDATE ITEM SET ITEMID = {0}, NAME = '{1}', TYPEITEM = '{2}', STOCK = '{3}', PRICE = '{4}')"
                 , item[0], item[1], item[2], item[3]
                 , item[4]);
             XCTNonQuery(query);
@@ -391,7 +393,7 @@ namespace DataLibrary
         {
             string dateStart = String.Format("TO_DATE('{0}', 'yyyy/mm/dd hh24:mi:ss')", rental[2]);
             string dateEnd = String.Format("TO_DATE('{0}', 'yyyy/mm/dd hh24:mi:ss')", rental[3]);
-            string query = String.Format("INSERT INTO RENTAL VALUES({0}{1}{2}{3}{4})"
+            string query = String.Format("INSERT INTO RENTAL VALUES('{0}','{1}',{2},{3},'{4}')"
                 , rental[0], rental[1], dateStart, dateEnd
                 , rental[4]);
             XCTNonQuery(query);
@@ -406,7 +408,7 @@ namespace DataLibrary
         {
             string dateStart = String.Format("TO_DATE('{0}', 'yyyy/mm/dd hh24:mi:ss')", rental[2]);
             string dateEnd = String.Format("TO_DATE('{0}', 'yyyy/mm/dd hh24:mi:ss')", rental[3]);
-            string query = String.Format("UPDATE RENTAL SET RENTALID = {0}, GUESTID = {1}, STARTDATE = {2}, ENDDATE = {3}, TOTALAMOUNT = {4})"
+            string query = String.Format("UPDATE RENTAL SET RENTALID = {0}, GUESTID = {1}, STARTDATE = {2}, ENDDATE = {3}, TOTALAMOUNT = '{4}')"
                 , rental[0], rental[1], dateStart, dateEnd
                 , rental[4]);
             XCTNonQuery(query);
@@ -437,9 +439,15 @@ namespace DataLibrary
             string query = "SELECT MAX(ITEMRENTALID) FROM ITEMRENTAL";
             result = XCTReader(query);
             int ID = Convert.ToInt32(result) + 1;
-            query = String.Format("INSERT INTO CATEGORY VALUES({0}{1}{2})"
+            query = String.Format("INSERT INTO CATEGORY VALUES({0},'{1}','{2}')"
                 , ID, irental[1], irental[2]);
             XCTNonQuery(query);
+        }
+        public List<Dictionary<string, string>> GetFileByFilePath(string filepath)
+        {
+            string query = String.Format("SELECT * FROM FileTable a, WHERE a.FILEPATH = '{0}'", filepath);
+            result = XCTReader(query);
+            return result;
         }
     }
 }
