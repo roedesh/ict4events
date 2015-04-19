f5398eadaa9eaebb6a7946800bed5e5d30d133a5
Berry Verschueren <berryverschueren@gmail.com>
2015-04-19 20:28:41 +0200

    pasword char system van ruud

--- ICT4Events/ReservationSystem/LoginForm.Designer.cs
+++ ICT4Events/ReservationSystem/LoginForm.Designer.cs
@@ -38,9 +38,10 @@
             // 
             // btLogin
             // 
-            this.btLogin.Location = new System.Drawing.Point(106, 140);
+            this.btLogin.Location = new System.Drawing.Point(141, 172);
+            this.btLogin.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btLogin.Name = "btLogin";
-            this.btLogin.Size = new System.Drawing.Size(75, 23);
+            this.btLogin.Size = new System.Drawing.Size(100, 28);
             this.btLogin.TabIndex = 0;
             this.btLogin.Text = "Login";
             this.btLogin.UseVisualStyleBackColor = true;
@@ -48,57 +49,64 @@
             // 
             // txtUsername
             // 
-            this.txtUsername.Location = new System.Drawing.Point(106, 68);
+            this.txtUsername.Location = new System.Drawing.Point(141, 84);
+            this.txtUsername.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(126, 20);
+            this.txtUsername.Size = new System.Drawing.Size(167, 22);
             this.txtUsername.TabIndex = 1;
             // 
             // lbUsername
             // 
             this.lbUsername.AutoSize = true;
-            this.lbUsername.Location = new System.Drawing.Point(42, 71);
+            this.lbUsername.Location = new System.Drawing.Point(56, 87);
+            this.lbUsername.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lbUsername.Name = "lbUsername";
-            this.lbUsername.Size = new System.Drawing.Size(58, 13);
+            this.lbUsername.Size = new System.Drawing.Size(77, 17);
             this.lbUsername.TabIndex = 2;
             this.lbUsername.Text = "Username:";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(29, 103);
+            this.label1.Location = new System.Drawing.Point(39, 127);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.Size = new System.Drawing.Size(90, 17);
             this.label1.TabIndex = 3;
             this.label1.Text = "Wachtwoord:";
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(107, 103);
+            this.txtPassword.Location = new System.Drawing.Point(143, 127);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(125, 20);
+            this.txtPassword.PasswordChar = '*';
+            this.txtPassword.Size = new System.Drawing.Size(165, 22);
             this.txtPassword.TabIndex = 4;
             // 
             // label2
             // 
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(13, 13);
+            this.label2.Location = new System.Drawing.Point(17, 16);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(259, 41);
+            this.label2.Size = new System.Drawing.Size(345, 50);
             this.label2.TabIndex = 5;
             this.label2.Text = "Reserveringssysteem";
             this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // LoginForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 186);
+            this.ClientSize = new System.Drawing.Size(379, 229);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.lbUsername);
             this.Controls.Add(this.txtUsername);
             this.Controls.Add(this.btLogin);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "LoginForm";
             this.Text = "Reserveringssysteem";
             this.ResumeLayout(false);
