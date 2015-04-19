34bc1f8df0a46ab5d30c1e0b32c7dddba5d53515
Berry Verschueren <berryverschueren@gmail.com>
2015-04-19 17:20:00 +0200

    toegevoegd van bestanden van map toevoegen

---
+++ ICT4Events/EventManagementSystem/MapForm.Designer.cs
@@ -0,0 +1,63 @@
+﻿namespace EventManagementSystem
+{
+    partial class MapForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pbMap = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pbMap)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pbMap
+            // 
+            this.pbMap.Location = new System.Drawing.Point(12, 12);
+            this.pbMap.Name = "pbMap";
+            this.pbMap.Size = new System.Drawing.Size(1163, 526);
+            this.pbMap.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pbMap.TabIndex = 0;
+            this.pbMap.TabStop = false;
+            // 
+            // MapForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1187, 550);
+            this.Controls.Add(this.pbMap);
+            this.Name = "MapForm";
+            this.Text = "MapForm";
+            this.Load += new System.EventHandler(this.MapForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbMap)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbMap;
+    }
+}
---
+++ ICT4Events/EventManagementSystem/MapForm.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EventManagementSystem
+{
+    public partial class MapForm : Form
+    {
+        public MapForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MapForm_Load(object sender, EventArgs e)
+        {
+            OpenFileDialog newdialog = new OpenFileDialog();
+            newdialog.Filter = "Jpg files or Png files (*.jpg)|*.jpg|(*.png)|*.png";
+            newdialog.InitialDirectory = @"C:\";
+            if (newdialog.ShowDialog() == DialogResult.OK)
+            {
+                pbMap.Image = Image.FromFile(newdialog.FileName);
+            }
+            this.Size = pbMap.Size;
+        }
+    }
+}
